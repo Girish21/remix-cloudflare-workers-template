c30711a76d059e8cfd6ddfd47d4f61560648e065
Girish21 <girishkumarv21@gmail.com>
2022-12-10 15:37:18 +0530

    🔥 enable streaming @edge

--- packages/remix-app/app/entry.server.tsx
+++ packages/remix-app/app/entry.server.tsx
@@ -1,21 +1,34 @@
-import type { EntryContext } from '@remix-run/cloudflare'
+import { renderToReadableStream } from 'react-dom/server'
 import { RemixServer } from '@remix-run/react'
-import { renderToString } from 'react-dom/server'
+import isbot from 'isbot'
+import type { EntryContext } from '@remix-run/cloudflare'
 
-export default function handleRequest(
+export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
-  let markup = renderToString(
+  const body = await renderToReadableStream(
     <RemixServer context={remixContext} url={request.url} />,
+    {
+      onError: error => {
+        responseStatusCode = 500
+        console.error(error)
+      },
+      signal: request.signal,
+    },
   )
 
-  responseHeaders.set('Content-Type', 'text/html')
+  if (isbot(request.headers.get('User-Agent'))) {
+    await body.allReady
+  }
 
-  return new Response('<!DOCTYPE html>' + markup, {
+  const headers = new Headers(responseHeaders)
+  headers.set('Content-Type', 'text/html')
+
+  return new Response(body, {
     status: responseStatusCode,
-    headers: responseHeaders,
+    headers,
   })
 }
