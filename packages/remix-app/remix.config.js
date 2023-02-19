38fa5e6545fb860af48729cb51ff142c4a618a2d
Girish21 <girishkumarv21@gmail.com>
2023-02-19 19:10:32 +0530

    tmp

--- packages/remix-app/remix.config.js
+++ packages/remix-app/remix.config.js
@@ -2,7 +2,12 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  serverBuildTarget: 'cloudflare-workers',
+  serverDependenciesToBundle: 'all',
+  serverMainFields: ['browser', 'module', 'main'],
+  serverMinify: true,
+  serverModuleFormat: 'esm',
+  serverPlatform: 'neutral',
   devServerBroadcastDelay: 1000,
   ignoredRouteFiles: ['**/.*'],
+  future: { unstable_dev: true },
 }
